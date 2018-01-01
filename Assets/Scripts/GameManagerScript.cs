6d4786bd788228e0cc5cd0b2ba1d6648eb013f75
RyoYamamoto <yamamoto@lifeistech.co.jp>
2018-01-01 17:33:06 +0900

    Initial Commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+	public GameObject mazePoint;
+	int layerNumber = 5;
+
+	// Use this for initialization
+	void Start () {
+		CreateMaze();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateMaze(){
+		//親オブジェクトを作って、そこから子供を3重ループで作る
+		//親オブジェクトの子供にしていく
+		GameObject mazeParent = new GameObject("MazeParent");
+		mazeParent.transform.position = Vector3.zero;
+
+		for(int i = 0; i < layerNumber; i++){
+			for(int j = 0; j < layerNumber; j++){
+				for(int k = 0; k < layerNumber; k++){
+					GameObject point = Instantiate(mazePoint, new Vector3(i * 2,j * 2,k * 2), Quaternion.identity);
+				}
+			}
+		}
+
+	}
+}
